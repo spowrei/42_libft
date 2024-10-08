963deece7a1341f23e1ba8d77da755635d50faa9
Mustafa Kaplan <mkaplan2005@gmail.com>
2024-10-08 22:50:44 +0300

    beginning

---
+++ libft.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukaplan <mukaplan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 22:27:31 by mukaplan          #+#    #+#             */
+/*   Updated: 2024/10/08 22:27:31 by mukaplan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+/*
+isalpha+
+isdigit+
+isalnum+
+isascii
+isprint
+strlen
+memset
+bzero
+memcpy
+memmove
+strlcpy
+strlcat
+toupper
+tolower
+strchr
+strrchr
+strncmp
+memchr
+memcmp
+strnstr
+atoi
+calloc
+strdup
+
+*/
+#endif
