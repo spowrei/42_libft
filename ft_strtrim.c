4d8abe3ebf71d1d437dac919feb65006a4ef77cb
Mustafa Kaplan <mkaplan2005@gmail.com>
2024-11-16 03:23:17 +0300

    updates

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,33 +1,28 @@
+
+//> const, işaretçinin gösterdiği verinin değiştirilemez olduğunu ifade eder.
+//> Eğer işaretçinin kendisinin değiştirilmesini istemiyorsanız, bu durumda const anahtarını hem işaretçinin gösterdiği veriye
+//> hem de işaretçiye uygulamalısınız: const char * const s1;
+
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set) //> sil-sil-mustafa sil-
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	char *begin_ptr;
-	char *end_ptr;
-	char *new_str;
-	int s1_len;
-	int set_len;
-	int new_str_len;
+	size_t	len;
+	char	*new_str;
 
-	if (s1 == NULL)
+	if(s1 == NULL)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	set_len = ft_strlen(set);
-	begin_ptr = s1;
-	while (ft_strnstr(begin_ptr, set, set_len) == begin_ptr)
-		begin_ptr += set_len;
-	end_ptr = &s1[s1_len - set_len - 1];
-	while (ft_strnstr(end_ptr, set, set_len) == end_ptr)
-		end_ptr -= set_len;
-	new_str_len = end_ptr - begin_ptr + 1;
-	new_str = (char *)malloc(sizeof(char) * (new_str_len + 1));
-	ft_strlcpy(new_str, begin_ptr, new_str_len);
+	while (*s1 !='\0' && ft_strchr(set, *s1) != NULL)
+		s1++;
+	len = ft_strlen(s1);
+	if(len==0)
+		return (ft_strdup(""));
+	while (len && s1[len - 1] && ft_strchr(set, s1[len - 1]) != NULL)
+		len--;
+	new_str = (char *)malloc(sizeof(char) * (len + 1));
+	if (new_str == NULL)
+		return (NULL);
+	ft_memcpy(new_str, s1, len);
+	new_str[len] = '\0';
 	return (new_str);
-}
-
-#include <stdio.h>
-int main()
-{
-	printf("%s", ft_strtrim("sil-sil-mustafa sil-", "sil-"));
-	return 0;
 }
--- libft.h
+++ libft.h
@@ -14,7 +14,7 @@
 # define LIBFT_H
 
 #include <stdlib.h> // size_t, NULL, malloc()
-// #include <unistd.h>
+#include <unistd.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -34,10 +34,10 @@ void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));	//!!
-void	ft_putchar_fd(char c, int fd);	//!!
-void	ft_putstr_fd(char *s, int fd);	//!!
-void	ft_putendl_fd(char *s, int fd);	//!!
-void	ft_putnbr_fd(int n, int fd);	//!!
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
@@ -45,7 +45,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);	//>   kontrol et
-char	*ft_strtrim(char const *s1, char const *set);	//!!   yapamaım
+char	*ft_strtrim(char const *s1, char const *set);	
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));	//!!   bu ne amk
 char	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
